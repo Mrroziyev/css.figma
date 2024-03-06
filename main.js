2fa3261b276c86c01789b62bdb12bdb3ef262e7c
azizroziyev <roziyevazizbek094@gmail.com>
2024-03-06 17:30:34 +0500

    Portfolio

---
+++ main.js
@@ -0,0 +1,11 @@
+let body = document.querySelector(body);
+let sun__btn = document.querySelector('.sun__btn');
+let moon__btn = document.querySelector('.moon__btn');
+
+sun__btn.addEventListener('click', function(){
+    body.classList.toggle('dark');
+})
+
+moon__btn.addEventListener('click', function(){
+    body.classList.toggle('dark');
+})
